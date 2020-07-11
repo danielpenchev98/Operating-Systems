05040a0618fef5be14bfad8677301a11d0422ba3
Daniel Penchev <42358431+danielpenchev98@users.noreply.github.com>
2020-07-11 14:46:06 +0300

    Fix formatting

--- homework3/main.c
+++ homework3/main.c
@@ -15,11 +15,11 @@ int main(int argc,char * argv[])
 
     if(strcmp(argv[1],"-h")==0)
     {
-      if(argc!=2)
-      {
-         errx(1,"\"-h\" doesnt accept any arguments");
-      }
-      HelpFunction();
+        if(argc!=2)
+        {
+            errx(1,"\"-h\" doesnt accept any arguments");
+        }
+        HelpFunction();
     	exit(0);
     }
     else if(argc<3)
@@ -32,8 +32,8 @@ int main(int argc,char * argv[])
     stat(argv[1],buffer);
     if(!S_ISREG(buffer->st_mode)&&strcmp(argv[2],"-c")==0)
     {
-      CreateConfigFile(argc,argv);
-      exit(0);
+        CreateConfigFile(argc,argv);
+        exit(0);
     }
     //If a file with that name exists, and the selected option is "-c" then the program will show an error and exit.
     else if(strcmp(argv[2],"-c")==0)
@@ -61,27 +61,26 @@ int main(int argc,char * argv[])
     struct PositionSegNum pos;
     if(strcmp(argv[2],"-s")==0||strcmp(argv[2],"-S")==0)
     {
-      if(argc!=5)
-      {
-      	errx(1,"Invalid number of arguments for the option \"-s/-S\"");
-      }
+        if(argc!=5)
+        {
+      	    errx(1,"Invalid number of arguments for the option \"-s/-S\"");
+        }
 
-     //HOW TO DELETE A VALUE - JUST USE "" as value to be set as a value on that particular parameter
-      pos=GetArgPosition(argv[3],&index);
-      if(pos.segnum==-1)
+        //HOW TO DELETE A VALUE - JUST USE "" as value to be set as a value on that particular parameter
+        pos=GetArgPosition(argv[3],&index);
+        if(pos.segnum==-1)
     	{
-    		  errx(5,"Sorry but this parameter isnt attached to any of the used segments");
+      	    errx(5,"Sorry but this parameter isnt attached to any of the used segments");
     	}
-      else if(strcmp(argv[4],"")!=0&&!Regex(argv[4],index))
-      {
-          errx(4,"Invalid value of argument to be assigned to that parameter");
-      }
-      else if(strcmp(argv[2],"-s")==0)
-      {
-          SetBit(content,pos,1);
-      }
-      SetValue(content,pos,argv[4]);
-    
+        else if(strcmp(argv[4],"")!=0&&!Regex(argv[4],index))
+        {
+            errx(4,"Invalid value of argument to be assigned to that parameter");
+        }
+        else if(strcmp(argv[2],"-s")==0)
+        {
+            SetBit(content,pos,1);
+        }
+        SetValue(content,pos,argv[4]);
     }
     else if(strcmp(argv[2],"-g")==0||strcmp(argv[2],"-G")==0)
     {
@@ -95,17 +94,16 @@ int main(int argc,char * argv[])
     	{
     		errx(5,"Sorry but this parameter isnt attached to any of the used segments");
     	}
-      else if(GetBit(content,pos)||strcmp(argv[2],"-G")==0)
-      {
-        PrintValue(content,pos);
-        printf(" - %s\n",argv[3]);
-      }
-    
+        else if(GetBit(content,pos)||strcmp(argv[2],"-G")==0)
+        {
+            PrintValue(content,pos);
+            printf(" - %s\n",argv[3]);
+        }
     }
     else if(strcmp(argv[2],"-l")==0||strcmp(argv[2],"-L")==0)
     {
     	int sz=0;
-      if(argc==3)
+        if(argc==3)
     	{ 
     		 sz=paramSize;
     	}
@@ -113,28 +111,28 @@ int main(int argc,char * argv[])
     	{
     		 sz=argc;    
     	}
-      //if argc is 3 => show all names of parameters, else show the value of the parameters listed after the 4rth element in the list
+        //if argc is 3 => show all names of parameters, else show the value of the parameters listed after the 4rth element in the list
     	int i=(argc==3 ? 0 : 3);
     	for(;i<sz;i++)
-      {
-          pos=GetArgPosition(argc==3 ? paramName[i] : argv[i],&index);
-          if(pos.segnum==-1)
+        {
+              pos=GetArgPosition(argc==3 ? paramName[i] : argv[i],&index);
+              if(pos.segnum==-1)
     		  {
     			    errx(5,"Sorry but this parameter isnt attached to any of the used segments");
     		  }
-          if(GetBit(content,pos)||strcmp(argv[2],"-L")==0)
-          {
-              if(argc!=3)
+              if(GetBit(content,pos)||strcmp(argv[2],"-L")==0)
               {
-                  PrintValue(content,pos);
-                  printf(" - %s\n",argv[i]);
+                    if(argc!=3)
+                    {
+                        PrintValue(content,pos);
+                        printf(" - %s\n",argv[i]);
+                    }
+                    else if(argc==3)
+                    {
+                        printf("%s\n",paramName[i]);
+                    }
               }
-              else if(argc==3)
-              {
-                  printf("%s\n",paramName[i]);
-              }
-          }
-      }
+        }
     }
     else if(strcmp(argv[2],"-b")==0)
     {
@@ -143,20 +141,20 @@ int main(int argc,char * argv[])
     	   	errx(1,"Invalid number of arguments for option \"-b\"");
     	 }
 
-       pos=GetArgPosition(argv[3],&index);
-       if(pos.segnum==-1)
+         pos=GetArgPosition(argv[3],&index);
+         if(pos.segnum==-1)
     	 {
     	  	errx(5,"Sorry but this parameter isnt attached to any of the used segments");
     	 }
 
-       printf("%d\n",index);
+         printf("%d\n",index);
     	 if(argv[4][1]!='\0'||(argv[4][0]!='0'&&argv[4][0]!='1'))
     	 {
     		 close(fd);
     		 errx(6,"Invalid bool value");
     	 }
     	 int val=atoi(argv[4]);
-       SetBit(content,pos,val);
+         SetBit(content,pos,val);
     }
     else 
     {
